98616b9ad2529c3bf5ad44df512241929cb96ffa
dvnilsen <dvnilsen@protonmail.com>
2023-03-27 15:32:38 -0700

    adds home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+export default function HomePage() {
+    return (
+        <h1>Welcome to the home page</h1>
+    );
+}
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,5 @@
-import { checkToken } from '../../utilities/users-service';
 
 export default function OrderHistoryPage() {
- 
   return (
     <>
       <h1>OrderHistoryPage</h1>
